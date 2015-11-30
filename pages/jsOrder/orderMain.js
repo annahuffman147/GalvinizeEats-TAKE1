036751fc6f0b66c318dc13ab5e2f9807b74cb6b5
annahuffman147 <annahuffman.plc@gmail.com>
2015-11-30 16:29:58 -0700

    stupid xmlrequest

--- pages/jsOrder/orderMain.js
+++ pages/jsOrder/orderMain.js
@@ -1,26 +1,37 @@
-
-var menuFull = new XMLHttpRequest();
-
-menuFull.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var menuListings = console.log(this.responseText);
+function pullData(){
+  var menuFull = new XMLHttpRequest();
+  menuFull.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var menuListings = console.log(this.responseText);
+    }
   }
+  menuFull.open('GET', 'https://galvanize-eats-api.herokuapp.com/menu');
+  menuFull.send();
 }
-
-menuFull.open('GET', 'https://galvanize-eats-api.herokuapp.com/menu');
-
-menuFull.send();
-
-function display() {
-  displayMenuItems()
-}
-display()
+pullData()
 
 
 
-function displayMenuItems (){
-  var menuItems = menuListings.data.menu.name
-  var menuPrices = data.menu.price
-  console.log(menuItems);
-  console.log(menuPrices);
-}
+function displayMenuItems(){
+  for (var i=0; i<menu.length; i++) {
+    var menuItem = pullData.menu[i].name
+      console.log(menuItem[i]);
+  // var menuPrice = data.menu.price
+  // $('#temperature').text(temperature)
+  // $('#largeTemp').text(temperature)
+};
+// displayMenuItems()
+//
+// function display() {
+//   displayMenuItems()
+// }
+// display()
+
+
+
+// function displayMenuItems (){
+//   var menuItems = menuListings.data.menu.name
+//   var menuPrices = data.menu.price
+//   console.log(menuItems);
+//   console.log(menuPrices);
+// }
