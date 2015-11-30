ae2d0793d725bd199b4e13807c7d86601a69593f
annahuffman147 <annahuffman.plc@gmail.com>
2015-11-30 12:26:22 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('#goToOrder').on('click', function(event){
+    event.preventDefault();
+    city = $('#city').val();
+    state = $('#state').val();
+    AJAX(city, state);
+    checkToSave()
+  })
+});
